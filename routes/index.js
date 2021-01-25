0cd67f429ea7cd0b07e4206085dd834745fada65
Leila Jara <bienvenidos.al.hades@gmail.com>
2021-01-25 02:58:18 -0300

    mercadoliebre

---
+++ routes/index.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var router = express.Router();
+const indexController = require('../controllers/indexController')
+
+router.get('/', indexController.index)
+module.exports = router;
